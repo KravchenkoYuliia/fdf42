b7a931a468323ca17a47441cd909011f6f0e8aaf
Yuliia Kravchenko <yukravch@paul-f3Ar2s1.clusters.42paris.fr>
2025-02-28 10:24:02 +0100

    define COS and SIN for isometric projection, add variables for new values after projection, add struct for drawing the points

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: yukravch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 18:23:47 by yukravch          #+#    #+#             */
-/*   Updated: 2025/02/24 11:39:41 by yukravch         ###   ########.fr       */
+/*   Updated: 2025/02/28 10:01:02 by yukravch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,10 @@
 #include "ft_printf.h"
 #include "get_next_line.h"
 #include <fcntl.h>
+#include <math.h>
+
+#define	COS_30	0.8660254
+#define	SIN_30	0.5
 
 //struct for every point of map
 typedef struct s_point{
@@ -32,6 +36,15 @@ typedef	struct	s_map{
 	int	width; //numbers in line (x-columns)
 	int	height; //number of lines (y-rows)
 	t_point**	matrix; //2D-array struct for stocking z-values (height and color of each x,y point)
+	int	x_proj;
+	int	y_proj;//points after transformation 3D(x, y, z) to 2D(x', y')
+
 } t_map;
+
+typedef struct s_mlx{
+	void*	ptr;
+	void*	win_ptr;
+	void*	img;
+} t_mlx;
 
 #endif
