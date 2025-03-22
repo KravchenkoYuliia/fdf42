ef4db6b4d5905fb2ec152de8c7b45fa56ae6146a
Yuliia Kravchenko <yukravch@paul-f4Ar1s2.clusters.42paris.fr>
2025-03-22 16:49:25 +0100

    file is written according to the norm

--- sources/exit.c
+++ sources/exit.c
@@ -6,31 +6,32 @@
 /*   By: yukravch <yukravch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 10:51:36 by yukravch          #+#    #+#             */
-/*   Updated: 2025/03/20 15:56:03 by yukravch         ###   ########.fr       */
+/*   Updated: 2025/03/22 16:48:57 by yukravch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-void    ft_exit_fd(int fd, char *line, t_map *map)
+void	ft_exit_fd(int fd, char *line, t_map *map)
 {
-    if (!line)
-        perror("Error: Line is NULL\n");
-    if (line)
-	    free(line);
-    if (fd)
-        close(fd);
-    if (map)
-	    ft_free_map(map, map->height);
-    exit(EXIT_FAILURE);
+	if (!line)
+		perror("Error: Line is NULL\n");
+	if (line)
+		free(line);
+	if (fd)
+		close(fd);
+	if (map)
+		ft_free_map(map, map->height);
+	exit (EXIT_FAILURE);
 }
 
-int	ft_exit_final(t_mlx* mlx, t_map* map, t_hook* hook)
+int	ft_exit_final(t_mlx *mlx, t_map *map, t_hook *hook)
 {
 	if (mlx->ptr && mlx->img)
 		mlx_destroy_image(mlx->ptr, mlx->img);
 	if (mlx->ptr && mlx->win_ptr)
-	{	mlx_clear_window(mlx->ptr, mlx->win_ptr);
+	{
+		mlx_clear_window(mlx->ptr, mlx->win_ptr);
 		mlx_destroy_window(mlx->ptr, mlx->win_ptr);
 	}
 	if (mlx->ptr)
@@ -41,7 +42,7 @@ int	ft_exit_final(t_mlx* mlx, t_map* map, t_hook* hook)
 	ft_free_map(map, map->height);
 	free(mlx);
 	free(hook);
-	return 0;
+	return (0);
 }
 
 void	ft_exit_perror(char *msg)
@@ -55,4 +56,3 @@ void	ft_exit_mlx(t_mlx *mlx)
 	free(mlx);
 	exit(EXIT_FAILURE);
 }
-
