2b865b845018d9bb8daa5e4c7918765237afe7c6
Yuliia Kravchenko <yukravch@paul-f4Ar1s2.clusters.42paris.fr>
2025-03-22 17:30:22 +0100

    separate bonus and mandatory program versions

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
 NAME = fdf
+NAME_BONUS = fdf_bonus
 CC = cc
 
 all: $(NAME)
@@ -6,14 +7,19 @@ all: $(NAME)
 SRC_DIR = sources
 OBJ_DIR = objects
 FILES = main.c draw_points.c fdf.c initialize.c calculation.c map.c hooks.c exit.c free.c
+FILES_BONUS = main.c draw_points.c fdf.c initialize.c calculation.c map.c exit.c free.c hooks_bonus.c 
 SRC = $(addprefix $(SRC_DIR)/, $(FILES))
 INC = includes
 
 OBJ = $(addprefix $(OBJ_DIR)/, $(FILES:.c=.o))
+OBJ_BONUS = $(addprefix $(OBJ_DIR)/, $(FILES_BONUS:.c=.o))
 CFLAGS = -Wall -Werror -Wextra -g3 -I $(INC)
 $(OBJ_DIR)/%.o: $(SRC_DIR)/%.c
 	@mkdir -p $(OBJ_DIR)
 	$(CC) $(CFLAGS) -I/usr/include -Imlx_linux -I/opt/X11/include -I/opt -c $< -o $@
+
+$(NAME_BONUS): $(OBJ_BONUS)
+	$(CC) $(CFLAGS) $(OBJ_BONUS) libraries/minilibx-linux/libmlx.a libraries/libft_full_library/libft.a -lXext -lX11 -lm -lz -ldl -lglfw -pthread -lm -o $(NAME_BONUS)
 
 $(NAME): $(OBJ)
 	$(CC) $(CFLAGS) $(OBJ) libraries/minilibx-linux/libmlx.a libraries/libft_full_library/libft.a -lXext -lX11 -lm -lz -ldl -lglfw -pthread -lm -o $(NAME)
@@ -21,11 +27,13 @@ $(NAME): $(OBJ)
 mac: $(OBJ)
 	$(CC) $(CFLAGS) $(OBJ) -I/opt/X11/include -I/opt -L/opt/X11/lib -o $(NAME) -lX11
 
+bonus: $(NAME_BONUS)
 
 clean:
 	rm -rf $(OBJ_DIR)
 
 fclean: clean
-	rm -f $(NAME)
+	rm -f $(NAME) $(NAME_BONUS)
+
 re: fclean all
 
--- sources/hooks_bonus.c
+++ sources/hooks_bonus.c
@@ -6,14 +6,67 @@
 /*   By: yukravch <yukravch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/05 11:00:00 by yukravch          #+#    #+#             */
-/*   Updated: 2025/03/22 17:21:09 by yukravch         ###   ########.fr       */
+/*   Updated: 2025/03/22 16:56:30 by yukravch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
+int	ft_is_zoom(int keycode, t_hook *param)
+{
+	if (keycode == 'i')
+	{
+		param->map->zoom *= 1.1;
+		return (1);
+	}
+	else if (keycode == 'o')
+	{
+		param->map->zoom /= 1.2;
+		return (1);
+	}
+	return (0);
+}
+
+int	ft_is_translate(int keycode, t_hook *param)
+{
+	if (keycode == 65361)
+	{
+		param->map->translate_x -= 10;
+		return (1);
+	}
+	else if (keycode == 65363)
+	{
+		param->map->translate_x += 10;
+		return (1);
+	}
+	else if (keycode == 65362)
+	{
+		param->map->translate_y -= 10;
+		return (1);
+	}
+	else if (keycode == 65364)
+	{
+		param->map->translate_y += 10;
+		return (1);
+	}
+	return (0);
+}
+
 int	ft_key_press(int keycode, t_hook *param)
 {
+	char	edit;
+
+	edit = 0;
+	if (ft_is_zoom(keycode, param))
+		edit = 1;
+	if (ft_is_translate(keycode, param))
+		edit = 1;
+	if (edit == 1)
+	{
+		ft_generate_image(param->mlx, param->map);
+		mlx_put_image_to_window(param->mlx->ptr,
+			param->mlx->win_ptr, param->mlx->img, 0, 0);
+	}
 	if (keycode == 65307)
 	{
 		mlx_loop_end(param->mlx->ptr);
