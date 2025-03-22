e2cc99d0ce035024e791939fc0177bbe63c70a3b
Yuliia Kravchenko <yukravch@paul-f4Ar1s2.clusters.42paris.fr>
2025-03-22 13:57:16 +0100

    new file for free

---
+++ sources/free.c
@@ -0,0 +1,61 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yukravch <yukravch@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/20 13:39:10 by yukravch          #+#    #+#             */
+/*   Updated: 2025/03/20 14:01:50 by yukravch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "fdf.h"
+
+void	ft_free_map(t_map *map, int allocated_rows)
+{
+	int	i;
+
+	i = 0;
+	if (!map)
+		return ;
+	if (map->matrix)
+	{
+		while (i < allocated_rows)
+		{
+			if (map->matrix[i])
+				free(map->matrix[i]);
+			i++;
+		}
+		free(map->matrix);
+	}
+	free(map);
+}
+
+void	*ft_free_2d_array(char **array)
+{
+	int	i;
+	int	word;
+
+	i = 0;
+	word = 0;
+	while (array[word])
+		word++;
+	while (i < word)
+	{
+		free(array[i]);
+		i++;
+	}
+	free(array);
+	return (NULL);
+}
+void	free_last_file_content(int fd)
+{
+	char* line;
+	line = get_next_line(fd);
+	while (line != NULL) {
+		free(line);
+		line = get_next_line(fd);
+	}
+	close(fd);
+}
