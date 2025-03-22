50d498ea89d95e4ebcf2857a4837d877ea7d5af6
Yuliia Kravchenko <yukravch@paul-f4Ar1s2.clusters.42paris.fr>
2025-03-22 17:15:01 +0100

    file is written according to the norm

--- includes/fdf.h
+++ includes/fdf.h
@@ -6,7 +6,7 @@
 /*   By: yukravch <yukravch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/22 18:23:47 by yukravch          #+#    #+#             */
-/*   Updated: 2025/03/22 16:33:48 by yukravch         ###   ########.fr       */
+/*   Updated: 2025/03/22 17:14:40 by yukravch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,11 @@
 # define WIN_HEIGHT 1080
 
 typedef struct s_fd{
-        int flc;
-        int cc;
-        int rows;
-        char *line;
-
-}       t_fd;
+	int		flc;
+	int		cc;
+	int		rows;
+	char	*line;
+}	t_fd;
 
 typedef struct s_point{
 	int		x;
@@ -67,7 +66,6 @@ typedef struct s_mlx{
 	char	*bits_buff;
 }	t_mlx;
 
-
 typedef struct s_hook {
 	t_mlx	*mlx;
 	t_map	*map;
@@ -78,11 +76,11 @@ int		check_if_fdf(char *filename);
 int		ft_count_line_numbers(char *str, char c);
 int		ft_key_press(int keycode, t_hook *param);
 void	ft_check_if_rectangle(int fd, t_fd *text);
-t_map   *ft_initialize_map(int fd, t_map *map);
+t_map	*ft_initialize_map(int fd, t_map *map);
 t_map	*ft_malloc_map(int rows, int columns, t_map *map);
 t_hook	*ft_hooks(t_mlx *mlx, t_map *map);
 void	ft_initialize_points(int fd, t_map *map);
-void    ft_initialize_mlx(t_map* map);
+void	ft_initialize_mlx(t_map *map);
 void	ft_fdf(char *filename);
 void	*ft_free_2d_array(char **array);
 void	ft_free_map(t_map *map, int allocated_rows);
