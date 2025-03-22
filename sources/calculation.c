073507e51e36f10f8dbb1ba5bec7d563e0be7e86
Yuliia Kravchenko <yukravch@paul-f4Ar1s2.clusters.42paris.fr>
2025-03-22 16:41:22 +0100

    file is written according to the norm

--- sources/calculation.c
+++ sources/calculation.c
@@ -6,7 +6,7 @@
 /*   By: yukravch <yukravch@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/04 17:16:05 by yukravch          #+#    #+#             */
-/*   Updated: 2025/03/22 16:34:31 by yukravch         ###   ########.fr       */
+/*   Updated: 2025/03/22 16:40:42 by yukravch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,18 @@
 
 #include "fdf.h"
 
-double  ft_get_scale(t_map* map)
+double	ft_get_scale(t_map *map)
 {
-        double  scale_x;
-        double  scale_y;
-        double  scale;
+	double	scale_x;
+	double	scale_y;
+	double	scale;
 
-        scale_x = (WIN_WIDTH / 2) /  map->width;
-        scale_y = (WIN_HEIGHT / 2) / map->height;
-        scale = fmin(scale_x, scale_y);
-        return (scale);
+	scale_x = (WIN_WIDTH / 2) / map->width;
+	scale_y = (WIN_HEIGHT / 2) / map->height;
+	scale = fmin(scale_x, scale_y);
+	return (scale);
 }
+
 void	ft_projection(t_map *map)
 {
 	int		i;
@@ -38,11 +39,11 @@ void	ft_projection(t_map *map)
 		j = 0;
 		while (j < map->width)
 		{
-				map->matrix[i][j].x_proj = (((map->matrix[i][j].x * map->zoom) - 
-					(map->matrix[i][j].y * map->zoom)) * cos(0.523599));
+				map->matrix[i][j].x_proj = (((map->matrix[i][j].x * map->zoom)
+						- (map->matrix[i][j].y * map->zoom)) * cos(0.523599));
 				map->matrix[i][j].x_proj += map->offset_x;
-				map->matrix[i][j].y_proj = (((map->matrix[i][j].x * map->zoom) + 
-						(map->matrix[i][j].y * map->zoom)) * sin(0.523599)
+				map->matrix[i][j].y_proj = (((map->matrix[i][j].x * map->zoom)
+						+ (map->matrix[i][j].y * map->zoom)) * sin(0.523599)
 					- map->matrix[i][j].z);
 				map->matrix[i][j].y_proj += map->offset_y;
 				j++;
